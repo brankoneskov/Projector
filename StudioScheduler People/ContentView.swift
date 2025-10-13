c7dd596d66c784396504a1fb5d8a14cdafcb2a50
Branko <plishani@mac.com>
2025-10-13 10:48:47 +0100

    Initial Commit

---
+++ StudioScheduler People/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudioScheduler People
+//
+//  Created by Branko Neskov on 13/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudioScheduler People/StudioScheduler_PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioScheduler_PeopleApp.swift
+//  StudioScheduler People
+//
+//  Created by Branko Neskov on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StudioScheduler_PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
